8a60d1b4d7c0baeb168e55f4367f72dc3a3435ea
P4pyk04L4 <nclscharr@gmail.com>
2025-02-03 15:26:37 +0100

    20250203-1525 - First commit

---
+++ src/main/java/fr/eni/eni_cda_enchere/EniCdaEnchereApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.eni_cda_enchere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniCdaEnchereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EniCdaEnchereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/eni/eni_cda_enchere/EniCdaEnchereApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.eni_cda_enchere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EniCdaEnchereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
