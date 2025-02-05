da0e5ef69866ca862a8547b67942b91b3cc7116f
HiroeIWMT <hiroeiwamoto1@gmail.com>
2025-02-05 01:57:21 +0100

    202502050156 ajout les methodes dans UtilisateurController ajout les fichiers -view-profil.html -view-profil.html

--- src/main/java/fr/eni/eni_cda_enchere/controller/UtilisateurController.java
+++ src/main/java/fr/eni/eni_cda_enchere/controller/UtilisateurController.java
@@ -2,15 +2,15 @@ package fr.eni.eni_cda_enchere.controller;
 
 import fr.eni.eni_cda_enchere.bll.UtilisateurService;
 import fr.eni.eni_cda_enchere.bo.Utilisateur;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/utilisateurs")public class UtilisateurController {
@@ -25,6 +25,36 @@ import java.util.List;
     public String findAll(Model model) {
         List<Utilisateur> utilisateurs = utilisateurService.findAll();
         model.addAttribute("utilisateurs", utilisateurs);
-        return "contexte/view-utilisateurs"; // utilisateur/list.html にデータを渡す
+        return "profil/view-utilisateurs";
     }
+
+    @GetMapping("/profilpseudo")
+    public String findByPseudo(@RequestParam String pseudo, Model model) {
+        Optional<Utilisateur> utilisateur = utilisateurService.findByPseudo(pseudo);
+
+            model.addAttribute("utilisateur", utilisateur.get());
+            return "profil/view-profil";
+    }
+
+    // /edit/{pseudo} mais il y a Get et Post differents
+    @GetMapping("/edit/{pseudo}")
+    public String showEditForm(@PathVariable String pseudo, Model model) {
+        Optional<Utilisateur> utilisateur = utilisateurService.findByPseudo(pseudo);
+        model.addAttribute("utilisateur", utilisateur.orElse(null));
+        return "profil/view-editProfil";
+    }
+
+    /*
+    @PostMapping("/edit/{pseudo}")
+    public String updateProfil(@PathVariable String pseudo, @Valid @ModelAttribute Utilisateur utilisateur, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "editprofil";
+        }
+        utilisateur.setPseudo(pseudo);
+        utilisateurService.updateUser(utilisateur);
+        return "redirect:/utilisateurs/profilpseudo?pseudo=" + pseudo;
+    }
+*/
+
+
 }
